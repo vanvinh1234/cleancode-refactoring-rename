0e708846e950c6bea1ce59053f33f497ec0d3e93
vanvinh1234 <vanvinhbk90hoangkim@gmail.com>
2019-09-16 10:13:39 +0700

    clean code

--- src/Calculator.php
+++ src/Calculator.php
@@ -1,24 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: phanluan
- * Date: 25/10/2018
- * Time: 00:02
- */
+const ADDITION = '+';
+const SUBTRACTION = '-';
+const MULTIPLICATION = '*';
+const DIVISION = '/';
 
 class Calculator
 {
-    public function calculate($a, $b, $o) {
-        switch ($o) {
-            case '+':
-                return $a + $b;
-            case '-':
-                return $a - $b;
-            case '*':
-                return $a * $b;
-            case '/':
-                if ($b != 0) {
-                    return $a / $b;
+    public function calculate($firstOperand, $secondOperand, $operator) {
+        switch ($operator) {
+            case ADDITION:
+                return $firstOperand + $secondOperand;
+            case SUBTRACTION:
+                return $firstOperand - $secondOperand;
+            case MULTIPLICATION:
+                return $firstOperand * $secondOperand;
+
+            case DIVISION:
+                if ($secondOperand != 0) {
+                    return $firstOperand / $secondOperand;
                 } else {
                     echo("Can not divide by 0");
                     break;
@@ -28,3 +27,7 @@ class Calculator
         }
     }
 }
+$calculator = new Calculator();
+echo $calculator->calculate(5,6,ADDITION);
+
+
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -1,29 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: phanluan
- * Date: 24/10/2018
- * Time: 23:43
- */
 
 require __DIR__ . "/../src/Calculator.php";
+
 use PHPUnit\Framework\TestCase;
 
 class CalculatorTest extends TestCase
 {
     public function testCalculateAdd()
     {
-        $a = 1;
-        $b = 1;
-        $o = '+';
+        $firstOperand = 1;
+        $secondOperand = 1;
+        $operator = ADDITION;
 
         $expected = 2;
 
         $calculator = new Calculator();
-        $result = $calculator->calculate($a, $b, $o);
+        echo $result = $calculator->calculate($firstOperand, $secondOperand, $operator);
         $this->assertEquals($expected, $result);
     }
-
-
-
 }
